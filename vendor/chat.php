4bcd1932f7276e9b5befe08a589851e8c5fc7427
valyamoro <moralek228@gmail.com>
2023-08-05 04:43:27 +0300

    psr

--- vendor/chat.php
+++ vendor/chat.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1); 
+declare(strict_types=1);
 session_start();
 
 if (empty($_SESSION['user'])) {
@@ -17,43 +17,35 @@ if (!file_exists(MESSAGES_FILE)) {
 }
 
 $fileText = file_get_contents(MESSAGES_FILE);
-$messagesArray = json_decode($fileText, true);
+$messages = json_decode($fileText, true);
 
-$message = $_POST["user_message"] ?? null;
-$login = $_SESSION["user"]["login"] ?? null;
+$message = $_POST['user_message'] ?? null;
+$login = $_SESSION['user']['login'] ?? null;
 
 if ($message && $login) {
-    $userMessage = htmlspecialchars(strip_tags(\trim($_POST['user_message'])));
+    $userMessage = htmlspecialchars(strip_tags(trim($_POST['user_message'])));
 
-    if(!empty($newMessage)) {
+    if (!empty($userMessage)) {
         $newMessage = [
             'user' => [
                 'login' => $login,
             ],
             'message' => $userMessage,
         ];
+        $messages[] = $newMessage;
+        file_put_contents(MESSAGES_FILE, json_encode($messages));
+
+		$_SESSION['success'] = 'Ваше сообщение добавлено!';
+
+        header('Location: ../view/chat.php');
+	    die;
+
+    } else {
+        $_SESSION['error'] = 'Error'; 
     }
-    $messages[] = $newMessage;
-    file_put_contents(MESSAGES_FILE, json_encode($messages));
 }
 
-// if (!empty($_POST['user_message'])) {
-// 	$message = htmlspecialchars(strip_tags(\trim($_POST['user_message'])));
-// 	if (!empty($message)) {
-// 		$userName = $_SESSION['login'];
-// 		$message = $userName . '|||' . $message . PHP_EOL;
-// 		file_put_contents(MESSAGES_FILE, $message, FILE_APPEND);
-// 		$_SESSION['success'] = 'Ваше сообщение добавлено!';
-// 	} else {
-// 		$_SESSION['error'] = 'Error';
-// 	}
 
-// 	header('Location: /chat');
-// 	die;
-// }
 
-// if (file_exists(MESSAGES_FILE)) {
-// 	$messages = file_get_contents(MESSAGES_FILE);
-// }
 
 ?>
--- view/chat.php
+++ view/chat.php
@@ -1,43 +1,35 @@
-<?php
-
-require '../vendor/chat.php';
-
-?>
+<?php require '../vendor/chat.php' ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Чат</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Чат</title>
 </head>
 <body>
-    <?php 
-    if (!empty($_SESSION['user'])) {
-        foreach ($messages as $message) {
-            echo "<p>" . $message['user']['login'] . "</p>";
-            echo "<p>" . $message['message'] . "</p>";
-            echo '-------------------------------------------';
-        }
-        echo '
-        <form method="post">
-            <input type="text" name="user_message">
-            <input type="submit">
-        </form>
-        <form action="logout.php" method="POST">
-            <input type="submit" value="Выйти">
-        </form>';
-    } else {
-        echo '
-        <form action="" method="POST">
-            <input name="login" placeholder="login">
-            <input name="password" type="password" placeholder="password">
-            <input type="submit">
-        </form>
-        <form action="logout.php" method="POST">
-            <input type="submit" value="Выйти">
-        </form>';
-    }
-    ?>
+<?php if (!empty($_SESSION['success'])): ?>
+	<?php echo $_SESSION['success']; ?>
+<?php unset($_SESSION['success']); ?>
+<?php endif; ?>
+<?php if (!empty($_SESSION['error'])): ?>
+	<?php echo $_SESSION['error']; ?>
+	<?php unset($_SESSION['error']); ?>
+<?php endif; ?>
+
+<?php  foreach($messages ?? [] as $message): ?>
+
+	<p><?php echo $login; ?></p>
+	<p><?php echo $message['message']; ?></p>
+	<p>---------------------------------------</p>
+    <?php  endforeach; ?>
+    <form method="post">
+        <input type="text" name="user_message">
+        <input type="submit">
+    </form>
+    <form action="logout.php" method="POST">
+      <input type="submit" value="Выйти">
+    </form>
+
 </body>
 </html>

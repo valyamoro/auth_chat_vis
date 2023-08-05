838aefebcdfafe429add60ca17a2cf020943ae00
valyamoro <moralek228@gmail.com>
2023-08-05 04:09:41 +0300

    ini

--- vendor/chat.php
+++ vendor/chat.php
@@ -1,12 +1,16 @@
 <?php
-require_once 'connect.php';
-session_start();
-const MESSAGES_FILE = "../data/messages.txt";
 
-if (!isset($_SESSION['user'])) {
+declare(strict_types=1); 
+session_start();
+
+if (empty($_SESSION['user'])) {
     $login = $_POST['login'];
     $_SESSION['user']['login'] = $login;
 }
+
+const MESSAGES_FILE = "../data/messages.txt";
+
+require_once 'connect.php';
 
 if (!file_exists(MESSAGES_FILE)) {
     file_put_contents(MESSAGES_FILE, "[]");
@@ -19,54 +23,37 @@ $message = $_POST["user_message"] ?? null;
 $login = $_SESSION["user"]["login"] ?? null;
 
 if ($message && $login) {
-    $userMessage = htmlspecialchars($_POST['user_message']);
+    $userMessage = htmlspecialchars(strip_tags(\trim($_POST['user_message'])));
 
-    $newMessage = [
-        'user' => [
-            'login' => $login,
-        ],
-        'message' => $userMessage,
-    ];
-
-    $messagesArray[] = $newMessage;
-    file_put_contents(MESSAGES_FILE, json_encode($messagesArray));
+    if(!empty($newMessage)) {
+        $newMessage = [
+            'user' => [
+                'login' => $login,
+            ],
+            'message' => $userMessage,
+        ];
+    }
+    $messages[] = $newMessage;
+    file_put_contents(MESSAGES_FILE, json_encode($messages));
 }
 
+// if (!empty($_POST['user_message'])) {
+// 	$message = htmlspecialchars(strip_tags(\trim($_POST['user_message'])));
+// 	if (!empty($message)) {
+// 		$userName = $_SESSION['login'];
+// 		$message = $userName . '|||' . $message . PHP_EOL;
+// 		file_put_contents(MESSAGES_FILE, $message, FILE_APPEND);
+// 		$_SESSION['success'] = 'Ваше сообщение добавлено!';
+// 	} else {
+// 		$_SESSION['error'] = 'Error';
+// 	}
+
+// 	header('Location: /chat');
+// 	die;
+// }
+
+// if (file_exists(MESSAGES_FILE)) {
+// 	$messages = file_get_contents(MESSAGES_FILE);
+// }
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Чат</title>
-</head>
-<body>
-    <?php 
-    if (isset($_SESSION['user']['login'])) {
-        foreach ($messagesArray as $message) {
-            echo "<p>" . $message['user']['login'] . "</p>";
-            echo "<p>" . $message['message'] . "</p>";
-            echo '-------------------------------------------';
-        }
-        echo '
-        <form method="post">
-            <input type="text" name="user_message">
-            <input type="submit">
-        </form>
-        <form action="logout.php" method="POST">
-            <input type="submit" value="Выйти">
-        </form>';
-    } else {
-        echo '
-        <form action="" method="POST">
-            <input name="login" placeholder="login">
-            <input name="password" type="password" placeholder="password">
-            <input type="submit">
-        </form>
-        <form action="logout.php" method="POST">
-            <input type="submit" value="Выйти">
-        </form>';
-    }
-    ?>
-</body>
-</html>
--- view/chat.php
+++ view/chat.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start(); 
+require '../vendor/chat.php';
 
 ?>
 
@@ -12,30 +12,31 @@ session_start();
     <title>Чат</title>
 </head>
 <body>
-    
     <?php 
-    if($_SESSION['user']){
-        foreach($messagesArray as $message) {
-            echo "<p>" . $login . "</p>";
-            echo "<p>" . $message['message'] . "</p>"; 
+    if (!empty($_SESSION['user'])) {
+        foreach ($messages as $message) {
+            echo "<p>" . $message['user']['login'] . "</p>";
+            echo "<p>" . $message['message'] . "</p>";
             echo '-------------------------------------------';
         }
-    echo '<form method="post">
-        <input type="text" name="user_message">
-        <input type="submit">
-      </form>'; 
-      echo '<form action="logout.php" method="POST">
-      <input type="submit" value="Выйти">
-      </form>'; 
-    } elseif(!$_SESSION['user']){
-        echo '<form action="" method="POST">
-        <input name="login" placeholder="login">
-        <input name="password" type="password" placeholder="password">
-        <input type="submit">
-        </form>'; 
-        echo '<form action="logout.php" method="POST">
-        <input type="submit" value="Выйти">
-        </form>'; 
+        echo '
+        <form method="post">
+            <input type="text" name="user_message">
+            <input type="submit">
+        </form>
+        <form action="logout.php" method="POST">
+            <input type="submit" value="Выйти">
+        </form>';
+    } else {
+        echo '
+        <form action="" method="POST">
+            <input name="login" placeholder="login">
+            <input name="password" type="password" placeholder="password">
+            <input type="submit">
+        </form>
+        <form action="logout.php" method="POST">
+            <input type="submit" value="Выйти">
+        </form>';
     }
     ?>
 </body>
